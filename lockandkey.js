5a221035fd0b7f0c62ef466c1014427c92325580
Kalen Wessel <kalen.wessel@gmail.com>
2017-12-09 23:48:10 +0000

    Stripping out majority of the work to allow for a simple web request login. This will allow my google home to answer the door using voice commands.

--- lockandkey.js
+++ lockandkey.js
@@ -1,14 +1,10 @@
 var express = require("express"),
     http = require("http"),
-    path = require("path"),
-    mongoose = require("mongoose"),
-    hash = require("./pass").hash;
-var adminexpress = require("express")
+    path = require("path")
 var fs = require("fs");
 require( "console-stamp" )( console, { pattern : "dd/mm/yyyy HH:MM:ss.l" } );
 
 var app = express()
-var admin = adminexpress()
 
 // Read in key:values from config file
 var data = fs.readFileSync("./lockandkeyconfig.json"),Config;
@@ -18,24 +14,6 @@ try {
   console.log("Error parsing config")
   console.log(err);
 }
-
-// Setup sendgrid
-var sendgrid  = require("sendgrid")(Config.sendgridkey);
-
-// Database and Model
-mongoose.connect("mongodb://localhost/lockandkey");
-var UserSchema = new mongoose.Schema({
-    active: Boolean,
-    username: String,
-    password: String,
-    salt: String,
-    hash: String,
-    lock: Boolean,
-    start_date: Date,
-    end_date: Date
-});
-
-var User = mongoose.model("users", UserSchema);
 
 // Middlewares and configurations
 var bodyParser = require("body-parser");
@@ -47,85 +25,21 @@ app.use("/", express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
 
-app.get("/", function (req, res) {
-  res.redirect("/login");
-});
-
-app.use(function (req, res, next) {
-  var err = req.session.error,
-      msg = req.session.success;
-  delete req.session.error;
-  delete req.session.success;
-  res.locals.message = "";
-  res.locals.session = req.session
-  if (err) res.locals.message = err;
-  if (msg) res.locals.message = msg;
-  
-  next();
-});
-
-
-// Helper Functions
-function authenticate(name, pass, fn) {
-  if (!module.parent) console.log("Attempting to authenticate %s", name);
-
-  User.findOne({
-    username: name, active: true
-  },
-
-  function (err, user) {
-    if (user) {
-      if (err) return fn(new Error("Cannot find user"));
-	hash(pass, user.salt, function (err, hash) {
-	  if (err) return fn(err);
-	  if (hash == user.hash) return fn(null, user);
-	    fn(new Error("Invalid password"));
-	});
-    } else {
-      return fn(new Error("Cannot find user"));
-    }
-  });
-}
-
-function sendMail(subject, body) {
-
-  sendgrid.send({
-    to:       Config.emailto,
-    from:     Config.emailfrom,
-    subject:  subject,
-    text:     body 
-  }, function(err, json) {
-    if (err) { return console.error(err); }
-    console.log(json);
-  });
-}
-function requiredAuthentication(req, res, next) {
-  if (req.session.user) {
-    next();
+app.get("/login", function (req, res) {
+  if (req.query.token == Config.secret_token) {
+    PickUpAndDialSix = true;
+    res.send('Accepted');
+    setTimeout(function() {
+      PickUpAndDialSix = false; 
+    }, 20000); //force delay of 20 seconds.
   } else {
-    req.session.error = "Access denied!";
-    res.redirect('/login');
+    res.send('Denied');
   }
-}
-
-function userExist(req, res, next) {
-  User.count({
-    username: req.body.username
-  }, function (err, count) {
-    if (count === 0) {
-      next();
-    } else {
-      req.session.error = "That username already exists."
-      res.redirect("/admin");
-    }
-  });
-}
+});
 
 var bodyParser = require("body-parser")
 // app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true}));  // to support URL-encoded bodies
-admin.use(session({secret: "1234567890QWERTY"}));
-admin.use(bodyParser.urlencoded({extended: true}));  // to support URL-encoded bodies
 
 
 var serialport = require("serialport");
@@ -203,158 +117,13 @@ serialport.list(function (err, ports) {
 
 
 var server = app.listen(Config.port, function () {
-
   var host = server.address().address
   var port = server.address().port
 
   console.log("Lock and Key - Public Interface Listening At http://%s:%s", host, port)
 })
 
-var adminserver = admin.listen(Config.adminport, function () {
-
-  var host = adminserver.address().address
-  var port = adminserver.address().port
-  
-  console.log("Lock and Key - Admin Interface Listening At http://%s:%s", host, port)
-})
-
-
-admin.use("/", express.static(__dirname + "/public"));
-admin.use(function (req, res, next) {
-  var err = req.session.error,
-      msg = req.session.success;
-  delete req.session.error;
-  delete req.session.success;
-  res.locals.message = "";
-  res.locals.session = req.session
-  if (err) res.locals.message = err;
-  if (msg) res.locals.message = msg;
-
-  next();
-});
-admin.set("views", __dirname + "/views");
-admin.set("view engine", "jade");
-admin.get("/", function(req, res) {
-  res.redirect("/admin");
-});
-
-
-//Run this periodically to see if we should re-lock the door (ie: set a flag to not answer and dial nine)
-setInterval(function(){
-  var setLock = function(){
-    User.update({ "lock": false }, { $set: { "lock":true } }, {multi: true}, function(err, result){
-      if ( err ) throw err;
-      console.log("Setting lock and turning off auto-answers");
-      PickUpAndDialSix = false;
-    });
-  }
-
-  var queryAccess = function() {
-    User.find(function(err, items) {
-      if ( err ) throw err;
-      items.forEach(function(item, index) {
-        if ((Date.now() > item.end_date) && item.lock == false) {
-	  setLock();
-        }
-      });
-    });
-  }
-  queryAccess();
-    
-}, 10*1000);      
-
-app.get("/login", function (req, res, next) {
-  if (req.session.user) {
-    res.redirect("/success")
-  } else {
-    res.render("login");
-  }
-});
-
-app.get("/logout", function (req, res) {
-  req.session.destroy(function () {
-    res.redirect("/");
-  });
-});
-
-app.get("/success", requiredAuthentication, function (req, res) {
-  res.render("success", {
-    username: req.session.user.username,
-    endDate: req.session.enddate
-  });
-});
-
-app.post("/login", function (req, res) {
-  authenticate(req.body.username, req.body.password, function (err, user) {
-    if (user) {
-      req.session.regenerate(function () {
-        var startDate = new Date();
-        var endDate = new Date(startDate.getTime() + Config.unlocktimeout*60000);
-        req.session.user = user;
-        req.session.enddate = endDate
-        req.session.success = "Authenticated as " + user.username + ", you have until: " + endDate + " to buzz up.";
-        var setUser = function(){
-	  User.update({username : user.username}, {$set:{start_date : startDate, end_date : new Date(endDate), lock : false}}, function(err, result){
-	    if ( err ) throw err;
-	    console.log("Updated: " + user.username);
-	  });
-	}
-       
-        // Send an email
-        sendMail("Succesful Login", user.username + " has just logged in"); 
-        // Turn On Auto Answer
-        PickUpAndDialSix = true;
-        setUser();
-        res.render("success", {
-          username: req.session.user.username,
-          endDate: req.session.enddate
-        });
-      });
-    } else {
-      req.session.error = "Authentication failed, please check your username and password.";
-      res.redirect("/login")
-    }
-  });
-});
-
 app.get("/ping", function(req, res) {
   res.render("ping");
 });
 
-admin.get('/admin', function(req, res) {
-  User.find( {active: true}, function(err, userlist) {
-    if (err) return console.error(err);
-    res.render("admin", {
-      "userlist" : userlist
-    }); 
-  });
-});
-
-admin.post("/disableuser", function (req, res) {
-  User.update({ username: req.body.username, active: true}, {$set:{active: false}}, function(err) {
-    if (err) throw err;
-    res.redirect("admin");
-  });
-});
-
-admin.post("/adduser", userExist, function (req, res) {
-  var password = req.body.password;
-  var username = req.body.username;
-
-  hash(password, function (err, salt, hash) {
-    if (err) throw err;
-      var user = new User({
-        active: true,
-        username: username,
-        salt: salt,
-        hash: hash,
-        lock: false,
-        start_date: new Date(),
-        end_date: new Date().getTime() + Config.unlocktimeout*60000
-      }).save(function (err, newUser) {
-        if (err) throw err;
-        req.session.succcess = "User added";
-        res.redirect('admin');
-      });
-  });
-});
